5dc06bcae6253c1166c0e21108edb1ba7fb74694
Sebin Albert <36889531+ZEBDRON@users.noreply.github.com>
2026-06-23 11:03:04 +0530

    Refactor Main class to use class-level ORDERS

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
 from shopping_cart import ShoppingCart
 
 class Main:
-    """
-    ***********************************************
-    * This is the driver code. Don't change it!!!
-    ***********************************************
-    """
+
+    ORDERS = [
+        "Regular Electronics:100:2 Books:20:1",
+        "Member Clothing:50:4",
+        "VIP Electronics:100:1 Clothing:50:2",
+        "Gold Books:20:1",
+        "Regular Toys:30:2"
+    ]
 
     @staticmethod
     def main():
-        orders = [
-            "Regular Electronics:100:2 Books:20:1",
-            "Member Clothing:50:4",
-            "VIP Electronics:100:1 Clothing:50:2",
-            "Gold Books:20:1",
-            "Regular Toys:30:2"
-        ]
-
         import sys
 
         if len(sys.argv) < 2:
             print("Arguments are required to run the program.")
             return
 
-        Main.handle(sys.argv[1], orders)
+        Main.handle(sys.argv[1])
 
     @staticmethod
-    def handle(input_str, orders):
-        for order in orders:
+    def handle(input_str):
+        for order in Main.ORDERS:
             if order == input_str:
-                # Forward input to ShoppingCart
-                shopping_cart_args = [order]
-                ShoppingCart.main(shopping_cart_args)
+                ShoppingCart.main([order])
                 return
 
 
